364b386d88c80e8ce4dbc38ba37d0950dc24270a
matosys <max@matosys.de>
2026-04-13 15:38:00 +0200

    feat(*): introduce new project structure separated by client / server

--- src/balderhub/http/lib/setup_features/__init__.py
+++ src/balderhub/http/lib/setup_features/__init__.py
@@ -1,6 +1,7 @@
-# TODO add classes / functions from module (if any)
-# TODO delete module if it has no sub elements
+from . import client
+from . import server
 
 __all__ = [
-
+    "client",
+    "server"
 ]
